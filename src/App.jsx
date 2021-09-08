787a3f0298406d86e01ab27746a7915a27cf1125
ElJohnnie <johnfollmer1995@gmail.com>
2021-09-08 14:56:33 -0300

    importando e iniciando o react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import './assets/css/base/base.css'
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import './assets/css/base/base.css';
+import Home from './paginas/Home';
+import Sobre from './paginas/Sobre';
 
 function App() {
+
   return (
   <>
-
+    <Router>
+      <Route path="/">
+        <Home></Home>
+      </Route>
+      <Route path="/sobre">
+        <Sobre></Sobre>
+      </Route>
+    </Router>
   </>
   )
 }
