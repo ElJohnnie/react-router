75e2e60008ebd8589591fd114db8fdba614dc64d
ElJohnnie <johnfollmer1995@gmail.com>
2021-09-08 15:44:46 -0300

    add link do react-router-dom

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Imagem from '../assets/img/doguito.svg';
 import '../assets/css/componentes/cabecalho.css';
+import { Link } from 'react-router-dom';
 const Cabecalho = () => {
     return(
         <header className="cabecalho container">
@@ -16,16 +17,16 @@ const Cabecalho = () => {
             <nav className="menu-cabecalho">
                 <ul className="menu-itens">
                     <li>
-                        <a className="menu-item menu-item--entrar" href="#">Entrar</a>
+                        <Link className="menu-item menu-item--entrar" to="#">Entrar</Link>
                     </li>
                     <li>
-                        <a className="menu-item" href="#">Produtos</a>
+                        <Link className="menu-item" to="#">Produtos</Link>
                     </li>
                     <li>
-                        <a className="menu-item" href="/">Blog</a>
+                        <Link className="menu-item" to="/">Blog</Link>
                     </li>
                     <li>
-                        <a className="menu-item" href="/sobre">Sobre</a>
+                        <Link className="menu-item" to="/sobre">Sobre</Link>
                     </li>
                 </ul>
             </nav>
